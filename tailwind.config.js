bac82d680fc50d595467d58d4ae0fc599153f27a
Yihui Tham <yihuitham@gmail.com>
2022-06-02 07:50:08 +0800

    debug grid gap

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
-    spacing: {
-      icon: "30px",
-    },
   },
   plugins: [require("tailwind-scrollbar-hide")],
 };
