fef148bdd0873b8fee350eb669133f4310ba3105
Yihui Tham <yihuitham@gmail.com>
2022-06-02 07:44:27 +0800

    add icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    spacing: {
+      icon: "30px",
+    },
   },
   plugins: [require("tailwind-scrollbar-hide")],
 };
